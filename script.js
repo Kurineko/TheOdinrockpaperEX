9ed102a7f2ae3c64c443f9694f0129ec19e2af39
Kurineko <kuri.10kurita@gmail.com>
2022-08-02 14:08:15 -0500

    made logic and returns rock paper or scissor succe

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 function getComputerChoice(){
-    let randomnum = Math.floor(Math.random()*100+1);
-    return console.log(randomnum);
+    switch(Math.floor(Math.random()*10+1)) {
+        case 1:
+        case 2:
+        case 3:
+            return text = "rock";
+            break;
+
+        case 4:
+        case 5:
+        case 6:
+            return text = "scissors";
+            break;
+
+        case 7:
+        case 8:
+        case 9:
+            return text = "paper";
+            break;
+
+        case 10:
+            return getComputerChoice();
+            break;
+    }
 } 
+console.log(getComputerChoice())
